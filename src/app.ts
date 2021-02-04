ede5bad52d1a48a3b766e3382b1ea969d780c31e
gxkai <30716965+gxkai@users.noreply.github.com>
2021-02-04 22:08:25 +0800

    1

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,9 @@ import { TYPES } from "./containers/types";
 import { injectable, inject } from "inversify";
 import {Application} from "./renderer";
 import * as Cull from 'pixi-cull'
-import {Ticker, Sprite, Texture, Graphics, FillStyle} from 'pixi.js'
+import {Ticker, Sprite, Texture, Graphics, FillStyle, TilingSprite} from 'pixi.js'
 import {Viewport} from "pixi-viewport";
+import PixiFps from "pixi-fps";
 @injectable()
 export class App {
   constructor(
@@ -11,9 +12,6 @@ export class App {
   ) {
   }
   async load() {
-    this.application.view.style.width = '500px'
-    this.application.view.style.height = '500px'
-
     document.body.appendChild(this.application.view);
     const viewport = this.application.stage.addChild(new Viewport(
         {
@@ -26,20 +24,36 @@ export class App {
         .drag({ clampWheel: true })
         .wheel({ smooth: 3 })
         .pinch()
-        .decelerate()
-        .setZoom(0.5, false)
+        .decelerate({minSpeed: 0, friction: 0, bounce: 0})
+        .setZoom(0.08, false)
+    // fps
+    this.application.stage.addChild(new PixiFps())
+    //
     const rectangle = Sprite.from(Texture.WHITE);
     rectangle.width = 300;
     rectangle.height = 300;
     rectangle.tint = 0xFF0000;
     viewport.addChild(rectangle);
-      rectangle.position.x = (this.application.view.width - rectangle.width * viewport.scaled)
-      rectangle.position.y = (this.application.view.height - rectangle.height * viewport.scaled)
-      console.log(rectangle)
-      const rectangle1 = Sprite.from(Texture.WHITE);
-      rectangle1.width = 1;
-      rectangle1.height = 1;
-      rectangle1.tint = 0xFFFFFF;
-      rectangle.addChild(rectangle1)
+      rectangle.position.x = (this.application.view.width - rectangle.width * viewport.scaled)/2
+      rectangle.position.y = (this.application.view.height - rectangle.height * viewport.scaled)/2
+    //
+      rectangle.addChild(new TilingSprite(
+          this.application.renderer.generateTexture(new Graphics().lineStyle(1, 0xffffff).
+          drawRect(0, 0, 49, 49).endFill(), 1, 1),
+          rectangle.width,
+          rectangle.height
+      ))
+    //
+    const cull = new Cull.Simple();
+    cull.addList(viewport.children);
+    cull.cull(viewport.getVisibleBounds());
+    Ticker.shared.add(() =>
+    {
+      if (viewport.dirty)
+      {
+        cull.cull(viewport.getVisibleBounds());
+        viewport.dirty = false;
+      }
+    });
   }
 }
